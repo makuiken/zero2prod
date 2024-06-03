c13c41e20f04759fb86a5e043d61e700898cfe2f
makuiken <mkuiken18@outlook.com>
2024-06-02 21:09:38 -0400

    Complete through chapter 5.4.6

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use sqlx::postgres::PgPool;
+use sqlx::postgres::{PgPoolOptions};
 use std::net::TcpListener;
-use secrecy::ExposeSecret;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 
@@ -14,8 +13,8 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_lazy_with(configuration.database.with_db());
 
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,6 +9,7 @@ pub struct FormData {
     name: String,
 }
 
+#[allow(clippy::async_yields_async)]
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, pool),
@@ -18,14 +19,9 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await
-    {
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        },
-        Err(_) => {
-            HttpResponse::InternalServerError().finish()
-        }
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
@@ -33,15 +29,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     name = "Saving new subscriber details in the database",
     skip(form, pool)
 )]
-pub async fn insert_subscriber(
-    pool: &PgPool,
-    form: &FormData,
-) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+            "#,
         Uuid::new_v4(),
         form.email,
         form.name,
